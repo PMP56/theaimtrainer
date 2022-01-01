353c86379ba107e22b520b240e463ca9dff2964d
PMP56 <prashannapaudel2@gmail.com>
2022-01-01 17:53:08 +0545

    Google analytics added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,15 @@ class MyDocument extends Document {
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
             <meta  name="description" content="Free Aim Trainer helps you improve your aim in any FPS game such as Valorant, Fortnite, Counter-Strike: GO, and Call of Duty. Practice and Test your Aim Performance Online. No Installation Required." />
             <meta key="keywords" name="keywords" content="the aim trainer, aim trainer, FPS trainer, FPS, improve aim, boost aim, practice targets, practice aiming, FPS games" />
+            {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-3TCZ1C8V8E"></script>
+            <script dangerouslySetInnerHTML={{
+                __html: `window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                      
+                gtag('config', 'G-3TCZ1C8V8E');`}}>
+            </script>
         </Head>
         <body>
           <Main />
